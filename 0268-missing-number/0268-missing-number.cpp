48f817189ad8d85150f1993e417bdb7083122e40
Neha Kumari <neetad958@gmail.com>
2026-08-24 20:32:21 +0530

    Time: 1 ms (24.53%), Space: 21.9 MB (38.74%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        int i = 0;
-        while(i<n){
-            int correctidx = nums[i];
-            if(correctidx==i || nums[i]==n) i++;
-            else swap(nums[i],nums[correctidx]);
+        int ans = n;
+
+        for (int i = 0; i < n; i++) {
+            ans = ans ^ i ^ nums[i];
         }
-        for(int i=0;i<n;i++){
-            if(nums[i]!=i) return i;
-        }
-        return n;
+
+        return ans;
     }
 };

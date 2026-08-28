a9c4fb1c7967615d13ef3f9a27a8dfa6056f8c87
Neha Kumari <neetad958@gmail.com>
2026-08-29 00:11:38 +0530

    Time: 0 ms (100%), Space: 27.3 MB (52.43%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -4,9 +4,7 @@ public:
         int i = 0;
         int j = s.size()-1;
         while(i<j){
-            int temp = s[i];
-            s[i] = s[j];
-            s[j] = temp;
+            swap(s[i],s[j]);
             i++;
             j--;
         

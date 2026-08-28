9e84fd024d90dd703ab51815ad5fec03edad1eda
Neha Kumari <neetad958@gmail.com>
2026-08-29 01:27:46 +0530

    Time: 0 ms (100%), Space: 10.2 MB (73.59%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -6,30 +6,27 @@ public:
 
         while (i < j) {
 
-            if ((s[i] == 'a' || s[i] == 'e' || s[i] == 'i' ||
-                 s[i] == 'o' || s[i] == 'u' || s[i] == 'A' ||
-                 s[i] == 'E' || s[i] == 'I' || s[i] == 'O' ||
-                 s[i] == 'U') &&
-                (s[j] == 'a' || s[j] == 'e' || s[j] == 'i' ||
-                 s[j] == 'o' || s[j] == 'u' || s[j] == 'A' ||
-                 s[j] == 'E' || s[j] == 'I' || s[j] == 'O' ||
-                 s[j] == 'U')) {
+            // i par vowel nahi hai
+            if (!(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' ||
+                  s[i] == 'o' || s[i] == 'u' || s[i] == 'A' ||
+                  s[i] == 'E' || s[i] == 'I' || s[i] == 'O' ||
+                  s[i] == 'U')) {
+                i++;
+            }
 
+            // j par vowel nahi hai
+            else if (!(s[j] == 'a' || s[j] == 'e' || s[j] == 'i' ||
+                       s[j] == 'o' || s[j] == 'u' || s[j] == 'A' ||
+                       s[j] == 'E' || s[j] == 'I' || s[j] == 'O' ||
+                       s[j] == 'U')) {
+                j--;
+            }
+
+            // dono vowel hain
+            else {
                 swap(s[i], s[j]);
                 i++;
                 j--;
-            }
-
-            else if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' ||
-                     s[i] == 'o' || s[i] == 'u' || s[i] == 'A' ||
-                     s[i] == 'E' || s[i] == 'I' || s[i] == 'O' ||
-                     s[i] == 'U') {
-
-                j--;
-            }
-
-            else {
-                i++;
             }
         }
 

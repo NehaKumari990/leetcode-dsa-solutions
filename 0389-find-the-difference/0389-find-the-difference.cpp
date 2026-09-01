d86c2fb62056e53ca55e4f80059c012c236d2eac
Neha Kumari <neetad958@gmail.com>
2026-09-02 02:51:03 +0530

    Time: 0 ms (100%), Space: 9.2 MB (51.18%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        unordered_map<char, int> mp;
+        char ans = 0;
 
-        // s ke characters ki frequency store karo
         for(char ch : s) {
-            mp[ch]++;
+            ans = ans ^ ch;
         }
 
-        // t ke characters check karo
         for(char ch : t) {
-            if(mp[ch] == 0) {
-                return ch;
-            }
-
-            mp[ch]--;
+            ans = ans ^ ch;
         }
 
-        return ' ';
+        return ans;
     }
 };
